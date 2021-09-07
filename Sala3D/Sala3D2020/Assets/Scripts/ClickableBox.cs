f9496743326e956a0916eb054c5199679cb61c15
Vidas <miguel.vidalarga@gmail.com>
2021-09-07 19:29:33 +0100

    Removed useless scrips,png and videos.Also added final videos on CU
    Removed "useless" info.
    Added new curricular unit videos.

--- Sala3D/Sala3D2020/Assets/Scripts/ClickableBox.cs
+++ Sala3D/Sala3D2020/Assets/Scripts/ClickableBox.cs
@@ -9,11 +9,7 @@ public class ClickableBox : MonoBehaviour {
 	
     public string SetVideoPlayerURL(string boxName)
 	{
-		return videoURL = $"Assets/Courses/EngInformatica/{boxName}.avi";
+		return videoURL = $"Assets/Courses/EngInformatica/{boxName}.mp4";
 	}
 	
 }
-//TODO: Quando o objeto perder as vidas, carregar o video no botão play.
-//após disparar ao botao o video começar a reproduzir.
-//no shootablebox.cs perde as vidas e carrega o video
-//no rayCastShootComplete.cs é que inicia/pausa/para o video
